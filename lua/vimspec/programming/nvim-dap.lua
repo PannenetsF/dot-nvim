4ec09a1cc9bff00882b24c3787debca0aa212fd8
Pannenets F <pannenets.f@foxmail.com>
2025-04-16 22:20:07 +0800

    feat(plugin): osv and nvim-dap

---
+++ lua/vimspec/programming/nvim-dap.lua
@@ -0,0 +1,9 @@
+--- nvim-dap, for debugging
+--- @module vimspec.programming.nvim-dap
+local M = {}
+
+M.spec = function()
+    return 'mfussenegger/nvim-dap'
+end
+
+return M
---
+++ lua/vimspec/programming/osv.lua
@@ -0,0 +1,9 @@
+--- OSV plugin, for config debugging 
+--- @module vimspec.programming.osv
+local M = {}
+
+M.spec = function()
+    return 'jbyuki/one-small-step-for-vimkind'
+end
+
+return M
