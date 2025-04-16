6389822363b268704657974ca8fb818f7c500752
Pannenets F <pannenets.f@foxmail.com>
2025-04-16 23:20:22 +0800

    feat(plugin): auto-save

---
+++ lua/vimspec/edition/auto-save.lua
@@ -0,0 +1,24 @@
+--- Auto-save configuration module for Neovim
+--- @module vimspec.edition.auto-save
+
+local opts = {
+	execution_message = {
+		message = function() -- message to print on save
+			return ""
+		end,
+		dim = 0.18,         -- dim the color of `message`
+		cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+	},
+}
+local M = {}
+M.setup = function()
+	require("auto-save").setup(opts)
+end
+
+M.spec = function()
+	return {
+		"Pocco81/auto-save.nvim",
+	}
+end
+
+return M
