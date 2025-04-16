33679165778776c291b7940c7c36410eaf2d0834
Pannenets F <pannenets.f@foxmail.com>
2025-04-16 22:33:03 +0800

    feat(plugin): lazygit

---
+++ lua/vimspec/programming/lazygit.lua
@@ -0,0 +1,22 @@
+--- LazyGit 
+--- @module vimspec.programming.lazygit
+local M = {}
+
+M.setup = function()
+    return {
+        "kdheepak/lazygit.nvim",
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    }
+end
+
+return M
