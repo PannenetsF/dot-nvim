3ca5049a6b1ea2720a146b1794a47ae217188bc0
Pannenets F <pannenets.f@foxmail.com>
2025-04-16 22:19:19 +0800

    refactor(lazy): update lazy management

--- lua/manager/load.lua
+++ lua/manager/load.lua
@@ -1,20 +1,34 @@
---- configure lazy.nvim 
---- if it is not installed yet, clone it from github
---- after installation, load all specs from `manager/plugins/` directory
+--- configure lazy.nvim
 --- @module manager.load
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
+
+local M = {}
+
+--- if it is not installed yet, clone it from github
+M.make_sure_lazy_installed = function()
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not (vim.uv or vim.loop).fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
+end
+
+--- after installation, load modules' spec from vimspec module
+M.load_modules = function()
+	require("lazy").setup({
+		require('vimspec').setup_lazy()
 	})
 end
-vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	require('vimspec').setup()
-})
+M.setup = function()
+	M.make_sure_lazy_installed()
+	M.load_modules()
+end
+
+M.setup()
--- lua/vimspec/init.lua
+++ lua/vimspec/init.lua
@@ -1,23 +1,39 @@
---- set the preference of every plugin 
+--- set the preference of every plugin
 --- @module vimspec
 
 local loader = require("utils.loader")
 local M = {}
 
-M.setup = function ()
+--- load all spec for lazy
+M.setup_lazy = function()
     found = {}
     this_file = debug.getinfo(1, "S").source:sub(2)
     this_dir = loader.get_dir(this_file)
     this_dir_lua = loader.get_lua_files_recursively(this_dir)
+    name2found = {}
     for _, file in ipairs(this_dir_lua) do
         relpath = loader.relative_path(file)
         mod = require(relpath)
         if mod.spec then
-            -- append to list 
+            -- append to list
+            print('spec found for: ' .. this_dir .. ' ' .. relpath)
             table.insert(found, mod.spec())
+            name2found[relpath] = mod
         end
     end
+    M.name2found = name2found
     return found
+end
+
+--- setup for every plugin
+M.setup = function()
+    -- name2found
+    -- print name
+    for name, mod in pairs(M.name2found) do
+        if mod.setup then
+            mod.setup()
+        end
+    end
 end
 
 return M
