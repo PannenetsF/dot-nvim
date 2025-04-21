bf2649a72c01bdef2ab43f6d9653dba1dc91b899
Pannenets F <pannenets.f@foxmail.com>
2025-04-21 20:52:27 +0800

    feat(plugin): openingh

---
+++ lua/vimspec/programming/openingh.lua
@@ -0,0 +1,23 @@
+--- Open in GH, goto the remote file link
+--- @module vimspec.programming.openingh
+local M = {}
+
+
+--- define the lsp-related keymaps
+M.sparse_key_map = {
+    { "go", ":OpenInGHFileLines! <CR>", desc = "Open in Perm Link GitHub", mode = "n" },
+    { "go", ":OpenInGHFileLines! <CR>", desc = "Open in Perm Link GitHub", mode = "v" },
+    { "gy", ":OpenInGHFileLines!+ <CR>", desc = "Copy Perm Link GitHub", mode = "n" },
+    { "gy", ":OpenInGHFileLines!+ <CR>", desc = "Copy Perm Link GitHub", mode = "v" },
+}
+
+M.spec = function()
+    return {
+        "almo7aya/openingh.nvim",
+        init = function()
+            vim.g.openingh_copy_to_register = true
+        end,
+    }
+end
+
+return M
