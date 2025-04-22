07526099ec2c103a4ad10513439f3dead1f39da5
Pannenets F <pannenets.f@foxmail.com>
2025-04-22 10:27:17 +0800

    docs: add submodule desc

--- init.lua
+++ init.lua
@@ -1,16 +1,20 @@
---- entry of nvim 
+--- entry of nvim
 ---@file init.lua
 ---@author Your Name
 
---- M carries the configuration for the neovim 
+--- M carries the configuration for the neovim
 local M = {}
 
 --- `configure_path` sets the path for the configuration files
-function M.configure_path() 
+function M.configure_path()
 	M.lua_conf_dir = vim.fn.stdpath("config") .. "/lua"
 end
 
---- `core_modules` set the core module of nvim 
+--- `core_modules` set the core module of nvim
+--- autocommands set up needed autocommands
+--- vimconf set up the basic nvim configuration, without any plugins
+--- manager set up the plugin manager lazy.nvim, which reads from vimspec
+--- vimspec describes how to install the plugins and configure them
 function M.set_core_modules()
 	M.core_modules = {
 		"autocommands/",
@@ -20,16 +24,16 @@ function M.set_core_modules()
 	}
 end
 
---- `load_module` loads lua module from the base dir 
----@param name string 
-function M.load_module(name) 
+--- `load_module` loads lua module from the base dir
+---@param name string
+function M.load_module(name)
 	if vim.endswith(name, ".lua") then
-		-- if it is a lua file 
+		-- if it is a lua file
 		local module_name, _ = string.gsub(name, "%.lua", "")
 		package.loaded[module_name] = nil
 		require(module_name)
 	elseif vim.endswith(name, "/") then
-		-- if it is a dir 
+		-- if it is a dir
 		name = name:sub(1, -2)
 		local module = require(name)
 		module.setup()
@@ -39,7 +43,7 @@ function M.load_module(name)
 end
 
 --- `setup` sets up the core modules
-function M.setup() 
+function M.setup()
 	M.configure_path()
 	M.set_core_modules()
 	for _, mod in ipairs(M.core_modules) do
