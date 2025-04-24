cc4f67f6f68290a2f307ae8364ae740dda8a86c8
fanyunqian <fanyunqian.0@bytedance.com>
2025-04-24 16:38:15 +0800

    feat(plugin): add vista

---
+++ lua/vimspec/navigation/vista.lua
@@ -0,0 +1,8 @@
+--- Use Vista to View and search LSP symbols, tags in Vim/NeoVim.
+--- @module vimspec.navigation.vista
+
+local M = {}
+M.spec = function()
+	return "liuchengxu/vista.vim"
+end
+return M
