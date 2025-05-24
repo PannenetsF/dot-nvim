90afcf82417b63ac2143a2dd0bf7512bd74a4ba8
fanyunqian <fanyunqian.0@bytedance.com>
2025-05-24 14:43:52 +0800

    feat(plugin): set copilot shortcut

--- lua/vimspec/programming/copilot.lua
+++ lua/vimspec/programming/copilot.lua
@@ -2,6 +2,13 @@
 --- @module vimspec.programming.copilot
 local M = {}
 
+M.setup = function()
+	vim.g.copilot_no_tab_map = true
+	vim.g.copilot_assume_mapped = true
+	vim.g.copilot_tab_fallback = ""
+	vim.api.nvim_set_keymap("i", "<C-E>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+end
+
 M.spec = function()
 	return { "github/copilot.vim" }
 end
