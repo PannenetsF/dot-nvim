b65a2c2763247541499fd36ac5d0cc68ab2c96f8
fanyunqian <fanyunqian.0@bytedance.com>
2025-05-24 14:35:34 +0800

    refactor: remove deprecated "get_clients"

--- lua/vimspec/ui/lualine.lua
+++ lua/vimspec/ui/lualine.lua
@@ -203,7 +203,7 @@ M.setup = function()
 				"location",
 				{
 					function()
-						local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+						local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
 						if #buf_clients == 0 then
 							return "LSP Inactive"
 						end
