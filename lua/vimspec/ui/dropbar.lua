ce394e42e405f65e150b086f207c1faf550c91fd
fanyunqian <fanyunqian.0@bytedance.com>
2025-05-24 14:53:21 +0800

    feat(plugin): add dropbar

---
+++ lua/vimspec/ui/dropbar.lua
@@ -0,0 +1,28 @@
+--- This module provides configuration and key mappings for the dropbar plugin
+--- @module vimspec.edition.dropbar
+
+local M = {}
+
+M.normal_key_map = {
+	b = {
+		s = { "<cmd>lua require('dropbar.api').pick()<cr>", "Focus on dropbar" },
+	},
+}
+
+M.sparse_key_map = {
+	{
+		"[;",
+		":lua require('dropbar.api').goto_context_start()<CR>",
+		desc = "Go to start of current context",
+		mode = "n",
+	},
+	{ "];", ":lua require('dropbar.api').select_next_context()<CR>", desc = "Select next context", mode = "n" },
+	{ "<Leader>;", ":lua require('dropbar.api').pick()<CR>", desc = "Pick symbols in winbar", mode = "n" },
+}
+
+M.spec = function()
+	return {
+		"Bekaboo/dropbar.nvim",
+	}
+end
+return M
