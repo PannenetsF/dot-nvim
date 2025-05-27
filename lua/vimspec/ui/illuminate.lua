393eff55dfb379be316bfcf8814c75999681bea3
Pannenets F <pannenets.f@foxmail.com>
2025-05-27 23:45:03 +0800

    feat(plugin): add illuminate for word highlight

---
+++ lua/vimspec/ui/illuminate.lua
@@ -0,0 +1,12 @@
+--- This module provides configuration and key mappings for the illuminate plugin
+--- @module vimspec.edition.illuminate
+
+local M = {}
+
+M.spec = function()
+	return {
+		"RRethy/vim-illuminate",
+	}
+end
+
+return M
