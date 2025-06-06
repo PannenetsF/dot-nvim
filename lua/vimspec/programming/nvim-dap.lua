15e3320715c3a8007785f3878593cc0251a11258
fanyunqian <fanyunqian.0@bytedance.com>
2025-06-06 23:26:29 +0800

    feat(plugin): add nvim-dap

--- lua/vimspec/programming/nvim-dap.lua
+++ lua/vimspec/programming/nvim-dap.lua
@@ -1,9 +1,150 @@
---- nvim-dap, for debugging
+--- nvim-dap for debugging
 --- @module vimspec.programming.nvim-dap
+local icons = require('utils.icons')
 local M = {}
 
+M.normal_key_map = {
+  d = {
+    name = "Debug",
+    t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
+    b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
+    c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
+    C = { "<cmd>lua require'dap'.run_to_cursor()<cr>", "Run To Cursor" },
+    d = { "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect" },
+    g = { "<cmd>lua require'dap'.session()<cr>", "Get Session" },
+    i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
+    o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
+    u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
+    p = { "<cmd>lua require'dap'.pause()<cr>", "Pause" },
+    r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Toggle Repl" },
+    s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
+    q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
+    U = { "<cmd>lua require'dapui'.toggle({reset = true})<cr>", "Toggle UI" },
+  },
+}
+
+
+local ui_config = {
+  icons = { expanded = "", collapsed = "", circular = "" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  -- Use this to override mappings for specific elements
+  element_mappings = {},
+  expand_lines = true,
+  layouts = {
+    {
+      elements = {
+        { id = "scopes",      size = 0.33 },
+        { id = "breakpoints", size = 0.17 },
+        { id = "stacks",      size = 0.25 },
+        { id = "watches",     size = 0.25 },
+      },
+      size = 0.33,
+      position = "right",
+    },
+    {
+      elements = {
+        { id = "repl",    size = 0.45 },
+        { id = "console", size = 0.55 },
+      },
+      size = 0.27,
+      position = "bottom",
+    },
+  },
+  controls = {
+    enabled = true,
+    -- Display controls in this element
+    element = "repl",
+    icons = {
+      pause = "",
+      play = "",
+      step_into = "",
+      step_over = "",
+      step_out = "",
+      step_back = "",
+      run_last = "",
+      terminate = "",
+    },
+  },
+  floating = {
+    max_height = 0.9,
+    max_width = 0.5, -- Floats will be treated as percentage of your screen.
+    border = "rounded",
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+    max_value_lines = 100, -- Can be integer or nil.
+  },
+}
+
+local dap_config = {
+  breakpoint = {
+    text = icons.ui.Bug,
+    texthl = "DiagnosticSignError",
+    linehl = "",
+    numhl = "",
+  },
+  breakpoint_rejected = {
+    text = icons.ui.Bug,
+    texthl = "DiagnosticSignError",
+    linehl = "",
+    numhl = "",
+  },
+  stopped = {
+    text = icons.ui.BoldArrowRight,
+    texthl = "DiagnosticSignWarn",
+    linehl = "Visual",
+    numhl = "DiagnosticSignWarn",
+  },
+}
+
+M.setup = function()
+  require('dapui').setup(ui_config)
+  vim.fn.sign_define("DapBreakpoint", dap_config.breakpoint)
+  vim.fn.sign_define("DapBreakpointRejected", dap_config.breakpoint_rejected)
+  vim.fn.sign_define("DapStopped", dap_config.stopped)
+end
+
+
 M.spec = function()
-	return "mfussenegger/nvim-dap"
+  return {
+    "mfussenegger/nvim-dap",
+    lazy = true,
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "mfussenegger/nvim-dap-python",
+      'theHamsta/nvim-dap-virtual-text',
+    },
+    config = function()
+      require("dap-python").setup()
+      dap = require('dap')
+      dap.configurations.c = {
+        {
+          name = "Launch",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtBeginningOfMainSubprogram = false,
+        } }
+      dap.configurations.cpp = dap.configurations.c
+      require("nvim-dap-virtual-text").setup()
+    end
+  }
 end
 
 return M
