a2a26bbbace075defa4bac908f939f4e8e72e751
fanyunqian <fanyunqian.0@bytedance.com>
2025-06-12 11:26:43 +0800

    fix(plugin): detect needed binary before im-select
    s

--- lua/vimspec/edition/im-select.lua
+++ lua/vimspec/edition/im-select.lua
@@ -1,15 +1,29 @@
 --- load im-select, for zh-en co-edition
 --- @module vimspec.edition.im-select
 local M = {}
+local util = require("utils.functions")
+
+function has_im_select()
+	if util.executable("macism") then
+		return true
+	end
+	if util.executable("fcitx-remote") or util.executable("fcitx5-remote") or util.executable("ibus") then
+		return true
+	end
+	return false
+end
 
 M.setup = function()
-	local im_select = require("im_select")
-	im_select.setup()
+	local loaded, im_select = pcall(require, "im_select")
+	if loaded then
+		im_select.setup()
+	end
 end
 
 M.spec = function()
 	return {
 		"keaising/im-select.nvim",
+		enabled = has_im_select(),
 	}
 end
 
