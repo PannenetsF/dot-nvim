0dffc7b4c2d37db081b24969d10812da0ea1dc2d
fanyunqian.1 <fanyunqian.1@bytedance.com>
2025-12-01 18:18:25 +0800

    feat(neogit): add neogit

---
+++ lua/vimspec/programming/neogit.lua
@@ -0,0 +1,26 @@
+--- load neogit
+--- @module vimspec.programming.neogit
+local M = {}
+
+M.spec = function()
+	return {
+		"NeogitOrg/neogit",
+		lazy = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+			"nvim-mini/mini.pick", -- optional
+			"folke/snacks.nvim", -- optional
+		},
+		cmd = "Neogit",
+		keys = {
+			{ "<localleader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+		},
+	}
+end
+
+return M
