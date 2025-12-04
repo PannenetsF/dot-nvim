ced341dea539fbef443d9b243d1e66392e51de90
fanyunqian.1 <fanyunqian.1@bytedance.com>
2025-12-04 23:35:16 +0800

    feat(line): update lualine

--- lua/vimspec/ui/lualine.lua
+++ lua/vimspec/ui/lualine.lua
@@ -133,10 +133,8 @@ M.setup = function()
 		options = {
 			icons_enabled = true,
 			theme = "auto",
-			-- component_separators = { left = "", right = "" },
-			-- section_separators = { left = "", right = "" },
+			component_separators = { left = "⏐", right = "⏐" },
 			section_separators = "",
-			component_separators = "",
 			disabled_filetypes = {},
 			always_divide_middle = true,
 		},
