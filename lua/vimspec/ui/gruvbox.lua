6b2c34db6addfcb255cbedd4dc6665a981e9b5b0
fanyunqian.1 <fanyunqian.1@bytedance.com>
2025-12-22 22:01:32 +0800

    ui(gruvbox): only

--- lua/vimspec/ui/gruvbox.lua
+++ lua/vimspec/ui/gruvbox.lua
@@ -2,41 +2,14 @@
 --- @module vimspec.ui.gruvbox
 local M = {}
 
-M.setup = function() end
+M.setup = function()
+	vim.opt.background = "dark" -- set this to dark or light
+	vim.cmd.colorscheme("gruvbox-material")
+end
 
 M.spec = function()
 	return {
-		"ellisonleao/gruvbox.nvim",
-		name = "gruvbox",
-		priority = 100,
-		flavour = "auto", -- latte, frappe, macchiato, mocha
-		background = { -- :h background
-			light = "latte",
-			dark = "mocha",
-		},
-		opts = {
-			terminal_colors = true, -- add neovim terminal colors
-			undercurl = true,
-			underline = true,
-			bold = true,
-			italic = {
-				strings = true,
-				emphasis = true,
-				comments = true,
-				operators = false,
-				folds = true,
-			},
-			strikethrough = true,
-			invert_selection = false,
-			invert_signs = false,
-			invert_tabline = false,
-			inverse = true, -- invert background for search, diffs, statuslines and errors
-			contrast = "hard", -- can be "hard", "soft" or empty string
-			palette_overrides = {},
-			overrides = {},
-			dim_inactive = false,
-			transparent_mode = false,
-		},
+		"sainnhe/gruvbox-material",
 	}
 end
 
