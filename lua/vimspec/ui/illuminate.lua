55ea3ac7d2d0eb66dda1c0f22f0641913e675605
fanyunqian.1 <fanyunqian.1@bytedance.com>
2026-05-09 13:59:32 +0800

    fix(ui): avoid illuminate treesitter provider

--- lua/vimspec/ui/illuminate.lua
+++ lua/vimspec/ui/illuminate.lua
@@ -6,6 +6,13 @@ local M = {}
 M.spec = function()
 	return {
 		"RRethy/vim-illuminate",
+		config = function()
+			require("illuminate").configure({
+				-- The treesitter provider still depends on nvim-treesitter.locals,
+				-- which is brittle with newer nvim-treesitter / Neovim releases.
+				providers = { "lsp", "regex" },
+			})
+		end,
 	}
 end
 
