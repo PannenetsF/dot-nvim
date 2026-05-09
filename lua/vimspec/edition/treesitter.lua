845a3903d1688e1a95a3ed7e4fe78597202badf8
fanyunqian.1 <fanyunqian.1@bytedance.com>
2026-05-09 13:59:52 +0800

    fix(treesitter): support neovim 0.12 plugin api

--- lua/vimspec/edition/treesitter.lua
+++ lua/vimspec/edition/treesitter.lua
@@ -3,8 +3,61 @@
 
 local M = {}
 
+local ensure_installed = {
+	"python",
+	"cpp",
+	"cuda",
+	"lua",
+	"vim",
+	"json",
+	"toml",
+	"vimdoc",
+	"c",
+	"bash",
+	"markdown",
+	"markdown_inline",
+	"query",
+}
+
+local highlight_filetypes = {
+	"python",
+	"cpp",
+	"cuda",
+	"lua",
+	"vim",
+	"help",
+	"json",
+	"jsonc",
+	"toml",
+	"c",
+	"sh",
+	"bash",
+	"markdown",
+	"query",
+}
+
 M.setup = function()
 	local nts = require("nvim-treesitter")
+	if type(nts.setup) == "function" and type(nts.install) == "function" then
+		nts.setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
+
+		pcall(function()
+			nts.install(ensure_installed)
+		end)
+
+		vim.api.nvim_create_augroup("UserTreesitter", { clear = true })
+		vim.api.nvim_create_autocmd("FileType", {
+			group = "UserTreesitter",
+			pattern = highlight_filetypes,
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+		return
+	end
+
 	local setup = nil
 	local ok, _ = pcall(require, "nvim-treesitter.config")
 	if ok then
@@ -18,21 +71,7 @@ M.setup = function()
 	-- assert
 	assert(setup, "nvim-treesitter setup not found")
 	setup({
-		ensure_installed = {
-			"python",
-			"cpp",
-			"cuda",
-			"lua",
-			"vim",
-			"json",
-			"toml",
-			"vimdoc",
-			"c",
-			"bash",
-			"markdown",
-			"markdown_inline",
-			"query",
-		},
+		ensure_installed = ensure_installed,
 		ignore_install = {}, -- List of parsers to ignore installing
 		highlight = {
 			enable = true, -- false will disable the whole extension
@@ -44,17 +83,8 @@ end
 M.spec = function()
 	return {
 		"nvim-treesitter/nvim-treesitter",
-		cmd = {
-			"TSInstall",
-			"TSUninstall",
-			"TSUpdate",
-			"TSUpdateSync",
-			"TSInstallInfo",
-			"TSInstallSync",
-			"TSInstallFromGrammar",
-		},
-		event = "VimEnter",
-		-- event = "VeryLazy",
+		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
 	}
 end
