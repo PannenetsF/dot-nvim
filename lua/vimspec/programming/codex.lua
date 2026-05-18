9b2ae9d25dfd7050748e7daf71ec032c03800d1f
fanyunqian.1 <fanyunqian.1@bytedance.com>
2026-05-18 15:10:13 +0800

    feat(codex): add file ref yank mappings

---
+++ lua/vimspec/programming/codex.lua
@@ -0,0 +1,62 @@
+--- Codex helpers
+--- @module vimspec.programming.codex
+
+local M = {}
+local utils = require("utils.functions")
+
+local function buffer_ref_path()
+	local path = vim.fn.expand("%:p")
+	if path == "" then
+		return nil
+	end
+
+	return path
+end
+
+local function copy_ref(ref)
+	vim.fn.setreg('"', ref)
+	utils.copy_to_clipboard({ ref }, "+")
+	vim.notify("Copied Codex ref: " .. ref, vim.log.levels.INFO)
+end
+
+local function copy_current_line_ref()
+	local path = buffer_ref_path()
+	if not path then
+		vim.notify("No file path for current buffer", vim.log.levels.WARN)
+		return
+	end
+
+	copy_ref(string.format("%s:%d", path, vim.fn.line(".")))
+end
+
+local function copy_visual_line_ref()
+	local path = buffer_ref_path()
+	if not path then
+		vim.notify("No file path for current buffer", vim.log.levels.WARN)
+		return
+	end
+
+	local start_line = vim.fn.line("v")
+	local end_line = vim.fn.line(".")
+	if start_line > end_line then
+		start_line, end_line = end_line, start_line
+	end
+
+	copy_ref(string.format("%s:%d-%d", path, start_line, end_line))
+end
+
+M.normal_key_map = {
+	y = {
+		name = "Yank",
+		f = { copy_current_line_ref, "Copy Codex file ref" },
+	},
+}
+
+M.visual_key_map = {
+	y = {
+		name = "Yank",
+		f = { copy_visual_line_ref, "Copy Codex file ref" },
+	},
+}
+
+return M
