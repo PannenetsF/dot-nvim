7360cc36424b79b99e8a9053efef11ba13d1caee
fanyunqian.1 <fanyunqian.1@bytedance.com>
2026-06-13 19:23:47 +0800

    Remove stale Neogit plenary dependency

--- lua/vimspec/programming/neogit.lua
+++ lua/vimspec/programming/neogit.lua
@@ -11,7 +11,6 @@ M.spec = function()
 		"NeogitOrg/neogit",
 		lazy = true,
 		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
 			"sindrets/diffview.nvim", -- optional - Diff integration
 
 			-- Only one of these is needed.
