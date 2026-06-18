8705a28ba4491eb899b8d47a884d6b580699ab98
fanyunqian.1 <fanyunqian.1@bytedance.com>
2026-06-18 23:36:28 +0800

    Tune Neovide UI settings

--- lua/vimconf/ui.lua
+++ lua/vimconf/ui.lua
@@ -8,6 +8,12 @@ M.conf = function()
 	vim.o.updatetime = 300 -- Reduce the time it takes to trigger the CursorHold event
 	vim.opt.colorcolumn = "80" -- Highlight column 80
 	vim.opt.cmdheight = 0
+
+	if vim.g.neovide then
+		vim.o.guifont = "UbuntuMono Nerd Font Mono:h16"
+		vim.g.neovide_cursor_animation_length = 0
+		vim.g.neovide_cursor_trail_size = 0
+	end
 end
 
 return M
